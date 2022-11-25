f2849ac075899a298ce20dfba9bd99e58e65dd5c
Rutvik Vaghasiya <vaghasiyarutvik716@gmail.com>
2022-11-25 20:42:10 +0530

    temp commit

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -2,6 +2,7 @@ import { MainControns } from './Components/MainControns';
 
 function App() {
   return (
+    // the flow of app starts here
     <div className="App">
       <MainControns />
     </div>
